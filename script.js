9e19128a263a41e5334043eecd73a297eeb26a06
Katelyn Mullen <katelynmullen@live.com>
2021-01-19 22:00:34 -0500

    Set up a delay between clicks

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 (function () {
-	const scrollBottom = () => { window.scrollTo(0,document.body.scrollHeight); }
-	const followAll = () => { document.querySelectorAll('[data-et-name=follow_user]').forEach(button => button.click()) }
-        scrollBottom();
-        followAll();
-	window.setInterval(scrollBottom, 10000);
-	window.setInterval(followAll, 25000);
+	const clickDelta = 500 //ms
+	const clickedButtons = new Set()
+	const scrollBottom = () => {
+		window.scrollTo(0, document.body.scrollHeight - 600);
+		setTimeout(followAll, 5000)
+	}
+	const followAll = () => {
+		const followButtons = document.querySelectorAll('[data-et-name=follow_user]')
+		const numButtons = followButtons.length
+		for (let i = 0; i < 1; i++) {
+			let delay = i * clickDelta;
+			setTimeout(() => {
+				const button = followButtons[i];
+				if(!clickedButtons.has(button) && button.getAttribute('data-et-name') == "follow_user") {
+					button.click();
+					debugger;
+					clickedButtons.add();
+				}
+			}, delay)
+		}
+		setTimeout(scrollBottom, clickDelta * numButtons)
+	}
+    scrollBottom();
 })();
