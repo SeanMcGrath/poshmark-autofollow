a17259309f488e289ae23cab10604d99c65c81c4
Katelyn Mullen <katelynmullen@live.com>
2021-01-19 22:09:27 -0500

    PR feedback

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
+// Wrap entire script in a function for isolated scope
 (function () {
-	const clickDelta = 500 //ms
-	const clickedButtons = new Set()
+
+	//increase to slow things down
+	const clickDelta = 500
+
+	// Scroll to bottom of page, wait 5 seconds for new users to load,
+	// then start the clicking process
 	const scrollBottom = () => {
 		window.scrollTo(0, document.body.scrollHeight - 600);
 		setTimeout(followAll, 5000)
 	}
+
 	const followAll = () => {
+		// find all follow buttons
 		const followButtons = document.querySelectorAll('[data-et-name=follow_user]')
 		const numButtons = followButtons.length
+
+		// set up clicks for all the buttons after appropriate delays
 		for (let i = 0; i < 1; i++) {
 			let delay = i * clickDelta;
-			setTimeout(() => {
-				const button = followButtons[i];
-				if(!clickedButtons.has(button) && button.getAttribute('data-et-name') == "follow_user") {
-					button.click();
-					debugger;
-					clickedButtons.add();
-				}
-			}, delay)
+			setTimeout(() => followButtons[i].click(), delay)
 		}
+
+		// set up another scroll-down cycle
 		setTimeout(scrollBottom, clickDelta * numButtons)
 	}
-    scrollBottom();
+
+	// start the process
+    followAll();
 })();
